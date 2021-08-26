c59c54be6f2a92c58e5ecb5d183e861e1646545b
Rolando Mamani Salas <Rolando.Mamani@fundacion-jala.org>
2021-08-25 21:27:40 -0400

    Modificando la respuesta del serviedor con un HTML

--- Modulo3/app.js
+++ Modulo3/app.js
@@ -1,6 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url,req.method, req.headers);
+    //process.exit()   Terminaremos el proceso por que no hay mas peticiones que atender
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My primera pagina</title></head>')
+    res.write('<body></h1>Hola mi primer Servidor en Node Js</h1></body>')
+    res.write('</html>')
+    res.end
+
 });
 server.listen(3000);
