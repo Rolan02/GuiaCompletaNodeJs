81db9f52a7da1a51bd4dd7a8bf95afebf5bb47fb
Rolando Mamani Salas <Rolando.Mamani@fundacion-jala.org>
2021-08-26 20:33:37 -0400

    Solicitudes de enrutamiento

--- Modulo3/app.js
+++ Modulo3/app.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url,req.method, req.headers);
+    const url = req.url;
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>Ingrese el mensaje</title></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</buttom></form></body>')
+        res.write('</html>')
+        return res.end();
+
+    }
     //process.exit()   Terminaremos el proceso por que no hay mas peticiones que atender
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
@@ -9,6 +17,5 @@ const server = http.createServer((req, res) => {
     res.write('<body></h1>Hola mi primer Servidor en Node Js</h1></body>')
     res.write('</html>')
     res.end
-
 });
 server.listen(3000);
