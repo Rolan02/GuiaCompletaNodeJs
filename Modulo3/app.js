eff16483c240b8c530565d836c922da0a4f732a6
Rolando Mamani Salas <Rolando.Mamani@fundacion-jala.org>
2021-08-26 20:46:19 -0400

    Solicitudes de requerimiento

--- Modulo3/app.js
+++ Modulo3/app.js
@@ -1,14 +1,21 @@
 const http = require('http');
-
+const fs =  require('fs');
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
     if(url === '/'){
         res.write('<html>');
         res.write('<head><title>Ingrese el mensaje</title></head>')
         res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</buttom></form></body>')
         res.write('</html>')
         return res.end();
+    }
 
+    if (url === '/message' && method === 'POST'){
+        fs.writeFileSync('message.txt' , 'ROLANDO');
+        res.statusCode = 302;
+        res.setHeader('Location','/');
+        return res.end();
     }
     //process.exit()   Terminaremos el proceso por que no hay mas peticiones que atender
     res.setHeader('Content-Type', 'text/html');
